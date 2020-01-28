b35975e66de585d3f88fd73d2ed6989228bc4aae
wemmelo <wellington.magno@gmail.com>
2020-01-27 22:39:15 -0300

    first commit

---
+++ src/ControleFluxo.java
@@ -0,0 +1,11 @@
+
+public class ControleFluxo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//dado o valor de um carro descubra em quantas vezes ele pode ser parcelado
+		//porém as parcelas não podem ser menores do que 1000
+	}
+
+}
