bdc4e39b51f4ce78778d36cef78b2f3bcd73fff5
w <w@192.168.1.4>
2020-03-27 22:34:20 -0300

    End of class 44

---
+++ src/br/com/abc/javacore/modificadorestatico/classes/Cliente.java
@@ -0,0 +1,36 @@
+package br.com.abc.javacore.modificadorestatico.classes;
+
+public class Cliente {
+	// sequencia de inicialização
+	//0° o bloco de inicialização estático é inicializado quando a JVM carregar a classe e apenas uma vez
+	//1° é alocado o espaço na memória para o objeto que será criado
+	//2° cada atributo de classe é criado e inicializado com seus valores default ou os valores esplicitados
+	//3° bloco de inicialização é executado
+	//4° o construtor é inicializado
+	private static int[] parcelas;
+    //bloco de inicialização
+	{
+		System.out.println("bloco de inicialização não estático");
+	}
+	//com o acrescimo do atributo static esse bloco de inicialização é executado apenas uma vez, independente 
+	//da quantidade de vezes que eu chamar a classe
+	static{
+		parcelas = new int[100];
+		System.out.println("dentro do bloco de inicialização estátivo");
+		for(int i=1; i<=100; i++) {
+			parcelas[i-1]=i;
+		}
+	}
+	static {
+		System.out.println("bloco de inicialização 2");
+	}
+	public Cliente() {
+		
+	}
+	
+	public static int[] getParcelas() {
+		return parcelas;
+	}
+
+		
+}
