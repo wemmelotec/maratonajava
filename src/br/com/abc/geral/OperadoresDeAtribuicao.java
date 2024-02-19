d9e704e914fff41b675c5167f555426c32d80e5a
unknown <tonmelo@hotmail.com>
2024-02-19 19:16:42 -0300

    rev operadores de atribuição

--- src/br/com/abc/geral/OperadoresDeAtribuicao.java
+++ src/br/com/abc/geral/OperadoresDeAtribuicao.java
@@ -2,11 +2,27 @@ package br.com.abc.geral;
 
 public class OperadoresDeAtribuicao {
     public static void main(String[] args) {
-        // =, -=, +=, *=, /=, %=
+        //Operadores de atribuição compostos =, -=, +=, *=, /=, %=
         int salario = 1000;
         //salario = salario + 1000;
-        salario += 1000;
+        salario += 1;
         System.out.println(salario);
+
+        int x = 10;
+        x -= 3;
+        System.out.println(x);
+
+        int y = 4;
+        y *= 2;
+        System.out.println(y);
+
+        int z = 10;
+        z /= 2;
+        System.out.println(z);
+
+        int w = 10;
+        z %= 3;
+        System.out.println(z);
 
         //status = idade < 18 ? <true> : <false>;
         String status = 20<18? "adulto": "criança";
