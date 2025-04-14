70805d237977703a8106a383cafffb06334ce674
wemmelotec <tonmelo@hotmail.com>
2025-04-14 09:51:20 -0300

    tratar exception

--- src/br/com/abc/geral/docs/cincopecados/BadExceptionHandling.java
+++ src/br/com/abc/geral/docs/cincopecados/BadExceptionHandling.java
@@ -1,4 +1,12 @@
 package br.com.abc.geral.docs.cincopecados;
 
 public class BadExceptionHandling {
+    public static void main(String[] args) {
+
+        try{
+            int resultado = 10/0;
+        }catch (Exception e){
+            //Engolindo a Exceção
+        }
+    }
 }
